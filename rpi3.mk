0ac98e4c0ca122653e0994eb7d98eaf39c4e88b2
Madhumitha Harishankar <madhumitha.harishankar@gmail.com>
2020-08-18 14:38:17 +0530

    RPI3 kernel flag

--- rpi3.mk
+++ rpi3.mk
@@ -138,6 +138,7 @@ linux-defconfig: $(LINUX_PATH)/.config
 
 LINUX_COMMON_FLAGS += ARCH=arm64
 
+KERNEL=7
 linux: linux-common
 	$(MAKE) -C $(LINUX_PATH) $(LINUX_COMMON_FLAGS) INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=$(MODULE_OUTPUT) modules_install deb-pkg
 
